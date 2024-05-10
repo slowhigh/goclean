19e57cde7f1288fec75d98dcd2bcc86419d78cd5
Jo YoHan <whdygks210@naver.com>
2024-05-10 18:56:03 +0900

    fix: MemoRepo interface

--- internal/usecase/interactor/repository.go
+++ internal/usecase/interactor/repository.go
@@ -8,10 +8,10 @@ import (
 
 type MemoRepo interface {
 	Exist(id int64) (bool, error)
-	FindOne(id int64) (*entity.Memo, error)
-	Count(start *time.Time, end *time.Time, keyword *string) (*int64, error)
-	FindAll(start *time.Time, end *time.Time, keyword *string, perPage int, page int) (*[]entity.Memo, error)
-	Create(newMemo entity.Memo) (*entity.Memo, error)
-	Update(id int64, newMemo entity.Memo) (*entity.Memo, error)
-	Delete(id int64) (*entity.Memo, error)
+	FindOne(id int64) (entity.Memo, error)
+	Count(start *time.Time, end *time.Time, keyword *string) (int64, error)
+	FindAll(start *time.Time, end *time.Time, keyword *string, perPage int, page int) ([]entity.Memo, error)
+	Create(newMemo entity.Memo) (entity.Memo, error)
+	Update(id int64, newMemo entity.Memo) (entity.Memo, error)
+	Delete(id int64) (entity.Memo, error)
 }
